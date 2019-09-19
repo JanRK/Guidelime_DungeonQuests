c889d3d01a409ac91bf8980b65384c7bd05310de
JanRK <jk@itmatic.dk>
2019-09-19 20:32:31 +0200

    Finish SM Library

--- 29-39_Scarlet_Monastery_Library.lua
+++ 29-39_Scarlet_Monastery_Library.lua
@@ -1,28 +1,22 @@
 Guidelime.registerGuide([[
 [D Quests for Scarlet Monastery - Library \\ https://github.com/JanRK/Guidelime_DungeonQuests]
 [GA Horde]
-[N 29-39 Scarlet Monastery Graveyard]
+[N 29-39 Scarlet Monastery Library]
 [NX 32-42 Scarlet Monastery Armory]
 
 
+There is a long quest chain *Test of Faith* that is not included, Step 8 in that chain is a quest to loot a book from Library [QA1160][O]
+Go to Thunder Bluff and accept [QA1049 Compendium of the Fallen] [G34.6,47.2 Thunder Bluff] [A Orc,Tauren,Troll]
 Go to Undercity and accept [QA1048 Into The Scarlet Monastery] [G56.2,92.6 Undercity]. Need to be level [XP33]
+If you done the quest in Razorfen Kraul - [QT1109 Going, Going, Guano!], pickup the followup [QA1113 Hearts of Zeal] [G48.6,69.4 Undercity]
 
+Go into the Library instance in Scarlet Monestary[G85.3,32.2 Tirisfal Glades], it's the one on the far right without a door.
+Complete the following quests inside: \\[QC1049 Compendium of the Fallen] - Last corner before the hallway that leads straight into the room with the boss (Circular room on the map). Click a book on the bookshelf to the left. \\[QC1113 Hearts of Zeal] - Loot the Scarlet mobs
+After you kill Arcanist Doan, remember to loot *The Scarlet Key* from the chest in his chamber.
+Killing Houndmaster Loksey completes part of Into The Scarlet Monastery, but you also need to clear both Armory and Cathedral before it's done.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Go to Undercity and turn in [QT1113 Hearts of Zeal] [G48.6,69.4 Undercity]
+Go to Thunder Bluff and turn in [QT1049 Compendium of the Fallen] [G34.6,47.2 Thunder Bluff] [A Orc,Tauren,Troll]
 
 
 ]], 'DungeonQuests - JanRK')
