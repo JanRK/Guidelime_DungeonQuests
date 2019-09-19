40b30a850ce6755a6d4bc86e342b91c0621b3ced
JanRK <jk@itmatic.dk>
2019-09-19 23:26:46 +0200

    ZF quests

---
+++ 42-46_Zul_Farrak.lua
@@ -0,0 +1,40 @@
+Guidelime.registerGuide([[
+[D Quests for Zul Farrak \\ https://github.com/JanRK/Guidelime_DungeonQuests]
+[GA Horde]
+[N 42-51 Zul Farrak]
+[NX 46-55 Maraudon]
+
+
+
+First quest is a chain that starts in The Hinterlands. \\ Accept quest [QA2933 Venom Bottles] [G23,57.7 The Hinterlands]
+Go to Hillsbrad Foothills and turn in [QT2933 Venom Bottles] [G61.4,19.2 Hillsbrad Foothills] \\ Accept followup [QA2934 Undamaged Venom Sac]
+Go back to The Hinterlands, and kill *Witherbark Broodguard* untill you complete [QC2934 Undamaged Venom Sac] [G35,64 The Hinterlands]
+This is a good time to make *Mallet of Zul'Farrak*. This requires killing allot of 47-52 elites. If someone in your group already has this item, you can ship this step. \\ Kill *Qiaga the Keeper* level 50 Elite[G47,66 The Hinterlands]\\ Go to Jintha Alor and use it on the altar there [G59,77 The Hinterlands]
+Go to Hillsbrad Foothills and turn in [QT2934 Undamaged Venom Sac] [G61.4,19.2 Hillsbrad Foothills] \\ Accept followup [QA2935 Consult Master Gadrin]
+Go to Durotar and turn in [QT2935 Consult Master Gadrin][G56,74.6 Durotar]\\ Accept followup [QA2936 The Spider God]
+
+
+Go to Orgrimmar and accept [QA2861 Tabethas Task][G38.4,85.8 Orgrimmar]
+Go to Dustwallow Marsh and turn in [QT2861 Tabethas Task][G46,57 Dustwallow Marsh] \\ Accept [QA2846 Tiara of the Deep]
+Go to Shimmering Flats in Thousand Needles and accept [QA2770 Gahzrilla][G78,77 Thousand Needles] \\ To complete this quest, someone in the group need a *Mallet of Zul'Farrak*. This item is obtained in The Hinterlands.
+Go to Tanaris and accept [QA3520 Screecher Spirits][G67,22.4 Tanaris]
+Go to Feralas and complete [QC3520 Screecher Spirits]
+Go to Tanaris and turn in [QT3520 Screecher Spirits][G67,22.4 Tanaris]\\ Accept followup [QA3527 The Prophecy of Mosharu]
+Accept [QA3042 Troll Temper][G51.4,28.6 Tanaris]
+Accept [QA2865 Scarab Shells][G51.6,26.8 Tanaris]
+Accept [QA2768 Divino-matic Rod][G52.4,28.4 Tanaris]
+
+
+To to Zul Farrak [G38,18 Tanaris] and complete:\\[QC2936 The Spider God] - Click the tablet in the same area as Theka the Martyr\\[QC2846 Tiara of the Deep] - Drops from Hydromancer Velratha\\[QC2770 Gahzrilla] - Use the *Mallet of Zul'Farrak* to ring the gong at the end of Zul'Farrak. This will summon Gahz'rilla\\[QC3042 Troll Temper] - Drops from trolls in ZF\\[QC2865 Scarab Shells] - Drops from Scarabs in ZF\\[QC2768 Divino-matic Rod] - Drops from Sergeant Bly\\[QC3527 The Prophecy of Mosharu] - 1 drops from Theka the Martyr and 1 drops from Hydromancer Velratha
+
+
+Turn in [QT3042 Troll Temper][G51.4,28.6 Tanaris]
+Turn in [QT2865 Scarab Shells][G51.6,26.8 Tanaris]
+Turn in [QT2768 Divino-matic Rod][G52.4,28.4 Tanaris]
+Turn in [QT3527 The Prophecy of Mosharu][G67,22.4 Tanaris] \\ You get a followup that ends in Sunken Temple, that will be a part of that guide.
+Go to Shimmering Flats in Thousand Needles turn in [QT2770 Gahzrilla][G78,77 Thousand Needles]
+Go to Dustwallow Marsh and turn in [QT2846 Tiara of the Deep][G46,57 Dustwallow Marsh]
+Go to Durotar and turn in [QT2936 The Spider God][G56,74.6 Durotar] \\ You get a followup to kill a level 55 Elite in Hillsbrad Foothills. This quest is not part of this Dungeon Guide.
+
+
+]], 'DungeonQuests - JanRK')
