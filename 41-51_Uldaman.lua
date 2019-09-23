a71ca7cb3f799803b0a8791d7258f75199e66f07
JanRK <jk@itmatic.dk>
2019-09-23 23:23:23 +0200

    Fix mistakes, and add more info.

--- 41-51_Uldaman.lua
+++ 41-51_Uldaman.lua
@@ -5,7 +5,7 @@ Guidelime.registerGuide([[
 [NX 42-51 Zul Farrak]
 
 
-Long neutral Questline, that lets to hand in Indurium Ore (quest items) for 6s for each delivery of 4 ores. \\ If you have not done any of these quests already, it is worth it to skip entire chain until after Indurium Ore quest... \\ Get Quests [QA1104 Salt Flat Venom] and [QA1105 Hardened Shells] in Thousand Needles [G78,77 Thousand Needles]
+Long neutral Questline, that lets to hand in Indurium Ore (quest items) for 6s for each delivery of 4 ores. *MINERS only* \\ If you have not done any of these quests already, it is worth it to skip entire chain until after Indurium Ore quest... \\ Get Quests [QA1104 Salt Flat Venom] and [QA1105 Hardened Shells] in Thousand Needles [G78,77 Thousand Needles]
 Do Quest [QC1104] and [QC1105] 
 Turn in Quest [QT1104] and [QT1105] [G78,77 Thousand Needles]
 Pickup [QA1106 Martek the Exiled] [G78,77 Thousand Needles]
@@ -26,25 +26,29 @@ Take Zeppelin to Undercity and accept [QA2342 Reclaimed Treasures][G62.6,48.4 Un
 Head to Badlands and pickup [QA2258 Badlands Reagent Run] [G2.6,46 Badlands] and complete it \ [QC2258]
 Turn in [QT2258 Badlands Reagent Run] [G2.6,46 Badlands] and pickup [QA2202 Uldaman Reagent Run]
 Pickup [QA709 Solution to Doom][G51.4,76.8 Badlands]
-pickup [QA2418 Power Stones] [G42.4,52.8 Badlands] This one can be farmed outside, but can be done inside Uldaman as well, so do that.
+Pickup [QA2418 Power Stones] [G42.4,52.8 Badlands]
 
-Go to ULDAMAN \\ Pickup [QA2280 The Platinum Discs] \\ Complete the following objectives: \\ [QC2418 Power Stones] \\ [QC709 Solution to Doom] \\ [QC2342 Reclaimed Treasures] \\ [QC2283 Necklace Recovery] \\ [QC2202 Uldaman Reagent Run] 
+Go to ULDAMAN \\ Before you enter, complete these 2 quests outside the entrace.\\ [QC709 Solution to Doom] \\ [QC2342 Reclaimed Treasures]
 
-Turn in [QT2418 Power Stones] [G42.4,52.8 Badlands]
-Turn in [QT709 Solution to Doom] [G51.4,76.8 Badlands]
+Inside, complete the following objectives: \\ [QC2283 Necklace Recovery] - Drops ramdomly from the Troggs \\ [QC2202 Uldaman Reagent Run] - Mushrooms around the instance. They respawn, so can be picked up when running out as well. \\ Pickup [QA2278 The Platinum Discs] - In the room after the last boss, click the dics, talk to the ghost, listen to the ghost.
+When done talking click the dics again to turn in [QT2278 The Platinum Discs] and accept [QA2280 The Platinum Discs].
+
+In Badlands, turn in [QT709 Solution to Doom] [G51.4,76.8 Badlands]
 Turn in [QT2202 Uldaman Reagent Run] [G2.6,46 Badlands]
-Turn in [QT2342 Reclaimed Treasures] [G62.6,48.4 Undercity]
-Turn in [QT2283 Necklace Recovery] [G59.4,36.8 Orgrimmar] and pickup [QA2284 Necklace Recovery take 2]. Completes inside Uldaman at Remains of a Paladin.
+Go to Undercity and turn in [QT2342 Reclaimed Treasures] [G62.6,48.4 Undercity]
+Go to Orgrimmar and turn in [QT2283 Necklace Recovery] [G59.4,36.8 Orgrimmar] and pickup [QA2284 Necklace Recovery take 2]. Completes inside Uldaman at Remains of a Paladin.
 Turn in [QT2280 The Platinum Discs] [G34.6,47.2 Thunder Bluff]
 Optionally turn in [QT1192 Indurium Ore][O] [G80,75.8 Thousand Needles] in Thousand Needles of you have over 4 Indurium Ore or skip.
 
-Go to ULDAMAN pt2 \\ Turn in [QT2284 Necklace Recovery take 2] and pickup the followup [QA2318 Translating the Journal] from the Remains of a Paladin.
+Go to ULDAMAN pt2 \\ Inside, turn in [QT2284 Necklace Recovery take 2] and pickup the followup [QA2318 Translating the Journal] from the Remains of a Paladin.
 
 Turn in [QT2318 Translating the Journal] [G2.6,46 Badlands] \\ Accept [QA2338 Translating the Journal] and complete [QT2338 Translating the Journal] again. \\ Accept [QA2339 Find the Gems and Power Source]
 
-Go to ULDAMAN pt3 - Complete the following objective: \\ [QC2339 Find the Gems and Power Source] \\ - Shattered Necklace Ruby is in the Shadowforge Stash in the room with Galgann Firehammer. When you loot the chest, two mobs will spawn \\ - Sapphire drops from Grim'lok \\ - The Topaz is in a Conspicuous Urn in the room with the three lost vikings (where you get the Gni'kiv Medallion) \\ - Power Source from Archaedas
+Go to ULDAMAN pt3 - Complete the following objective: \\ [QC2339 Find the Gems and Power Source] \\ - The Topaz is in a Conspicuous Urn in the room with the three lost vikings (where you get the Gni'kiv Medallion) \\ - Shattered Necklace Ruby is in the Shadowforge Stash in the room with Galgann Firehammer. When you loot the chest, two mobs will spawn \\ - Sapphire drops from Grim'lok \\ - Power Source from Archaedas
+If you still haven't complete [QC2418 Power Stones], farm it outside the instance.
 
-Turn in [QT2339 Find the Gems and Power Source] [G2.6,46 Badlands] and accept [QA2340 Deliver the Gems]
+In Badlands, turn in [QT2339 Find the Gems and Power Source] [G2.6,46 Badlands] and accept [QA2340 Deliver the Gems]
+Turn in [QT2418 Power Stones] [G42.4,52.8 Badlands]
 Go to Orgrimmar and turn in [QT2340 Deliver the Gems] [G59.4,36.8 Orgrimmar] and accept [QA2341 Necklace Recovery Take 3]
 Head to Badlands and turn in [QT2341 Necklace Recovery Take 3] [G2.6,46 Badlands] \\ And you are *done!*
 
