518d214f17ce87d4ecdd91fd3254fc0ac63c2908
JanRK <jk@itmatic.dk>
2019-09-26 21:33:30 +0200

    Add rfc, and update to 0.9

---
+++ 13-18_Ragefile_Chasm.lua
@@ -0,0 +1,33 @@
+Guidelime.registerGuide([[
+[D Quests for Ragefile Chasm \\ https://github.com/JanRK/Guidelime_DungeonQuests]
+[GA Horde]
+[N 13-18 Ragefile Chasm]
+[NX 17-24 Wailing Caverns]
+
+
+Go to Undercity and accept [QA5725 The Power to Destroy...] [G56.2,92.6 Undercity]
+Go to Rahauro in Thunder Bluff and accept [QA5722 Searching for the Lost Satchel] and [QA5723 Testing an Enemys Strength][G70.4,32.2 Thunder Bluff]
+Go to Thrall in Orgrimmar and accept [QA5726 Hidden Enemies] [G32,37.8 Orgrimmar]
+
+Complete [QC5726 Hidden Enemies]
+
+Go to Thrall in Orgrimmar and turn in [QT5726 Hidden Enemies] [G32,37.8 Orgrimmar], accept followup [QA5727 Hidden Enemies]
+
+Complete [QC5727 Hidden Enemies] by talking to Neeru Fireblade [G49.6,50.4 Orgrimmar]
+Accept [QA5761 Slaying the Beast] from Neeru Fireblade.
+
+Go to Thrall in Orgrimmar and turn in [QT5727 Hidden Enemies] [G32,37.8 Orgrimmar], accept followup [QA5728 Hidden Enemies]
+
+
+
+Go to Ragefire Chasm and complete:\\ [QC5728 Hidden Enemies] - Kill Bazzalan and Jergosh the Invoker \\ [QC5761 Slaying the Beast] Kill Taragaman the Hungerer and loot his heart\\ Turn in [QT5722 Searching for the Lost Satchel] at Maur Grimtotem and accept followup [QA5724 Returning the Lost Satchel]\\ [QC5723 Testing an Enemys Strength] Kill 8 Ragefire Troggs and Shamans\\ [QC5725 The Power to Destroy...] Drops from Searing Blade Cultist and Warlocks
+
+
+Go to Thrall in Orgrimmar and turn in [QT5728 Hidden Enemies] [G32,37.8 Orgrimmar], accept followup [QA5729 Hidden Enemies]
+Go to Neeru Fireblade [G49.6,50.4 Orgrimmar] and turn in [QT5729 Hidden Enemies] and [QT5761 Slaying the Beast], accept followup [QA5730 Hidden Enemies]
+Go to Thrall in Orgrimmar and turn in [QT5730 Hidden Enemies] [G32,37.8 Orgrimmar]
+Go to Rahauro in Thunder Bluff and turn in [QT5724 Returning the Lost Satchel] and [QT5723 Testing an Enemys Strength] [G70.4,32.2 Thunder Bluff]
+Go to Undercity and turn in [QT5725 The Power to Destroy...] [G56.2,92.6 Undercity]
+
+
+]], 'DungeonQuests - JanRK')
