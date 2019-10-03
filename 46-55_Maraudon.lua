36ef5c6ba3a73e391123b1bdc79b6c53c86e106d
JanRK <jk@itmatic.dk>
2019-10-04 01:13:27 +0200

    Add Maraudon.

---
+++ 46-55_Maraudon.lua
@@ -0,0 +1,36 @@
+Guidelime.registerGuide([[
+[D Quests for Maraudon \\ https://github.com/JanRK/Guidelime_DungeonQuests]
+[GA Horde]
+[N 46-55 Maraudon]
+[NX 50-56 The Temple of Atal Hakkar]
+
+
+
+
+Go to Orgrimmar and accept [QA7068 Shadowshard Fragments][G39,86 Orgrimmar]
+Go to Desolace and accept [QA7029 Vyletongue Corruption][G23.2,70.2 Desolace]
+Accept [QA7064 Corruption of Earth and Seed][G26.8,77.6 Desolace]
+Accept [QA7067 The Pariahs Instructions][G50.4,86.6 Desolace] from Centaur Pariah who patrols between (43, 85) and (50, 86),
+Accept [QA7028 Twisted Evils][G62.2,39.6 Desolace]
+
+
+
+Go to Maraudon and\\Accept:\\[QA7044 Legends of Maraudon] From Cavandra in the orange section\\Complete:\\[QC7068 Shadowshard Fragments] Drops from Shadowshard Smashers and Rumblers on the purple side *outside*\\[QC7029 Vyletongue Corruption] Fill the Vial on the orange side *outside* the instance, then use the Filled Cerulean Vial on the Vylestem Vines (small mushrooms) inside (spawns Noxxious Scion elementals) \\[QC7028 Twisted Evils] Kill Demons on the Purple side\\[QC7067 The Pariahs Instructions] Kill The Nameless Prophet *outside*, and then kill the 5 Kahns (Use the amulet on the ghosts. 3 Khans *outside*, 2 inside)\\[QC7044 Legends of Maraudon] Get Diamond from Lord Vyletongue, Rod from Noxxion, and then kill Celebras the Cursed\\[QC7064 Corruption of Earth and Seed] Kill Princess Theradras, loot Seed of Life that starts quest that takes you to Moonglade\\Turn in:\\[QT7044 Legends of Maraudon] at Celebras the Redeemed
+Accept [QA7046 The Scepter of Celebras] from Celebras the Redeemed after killing Celebras the Cursed\\Turn in [QT7046 The Scepter of Celebras] at Celebras the Redeemed
+
+
+
+
+
+Turn in [QT7028 Twisted Evils][G62.2,39.6 Desolace]
+Turn in [QT7067 The Pariahs Instructions][G50.4,86.6 Desolace] to the Centaur Pariah who patrols between (43, 85) and (50, 86),
+Turn in [QT7064 Corruption of Earth and Seed][G26.8,77.6 Desolace]
+Go to Desolace and turn in [QT7029 Vyletongue Corruption][G23.2,70.2 Desolace]
+Go to Orgrimmar and turn in [QT7068 Shadowshard Fragments][G39,86 Orgrimmar]
+
+
+
+
+
+
+]], 'DungeonQuests - JanRK')
