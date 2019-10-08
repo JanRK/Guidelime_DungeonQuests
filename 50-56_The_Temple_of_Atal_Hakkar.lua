5be08b602675310a354f9baf27d3eaf92fd91631
JanRK <jk@itmatic.dk>
2019-10-09 00:56:59 +0200

    Add Sunken Temple

---
+++ 50-56_The_Temple_of_Atal_Hakkar.lua
@@ -0,0 +1,39 @@
+Guidelime.registerGuide([[
+[D Quests for Sunken Temple / The Temple of Atal Hakkar\\ https://github.com/JanRK/Guidelime_DungeonQuests]
+[GA Horde]
+[N 50-56 The Temple of Atal Hakkar]
+[NX 52-60 Blackrock Depths]
+
+
+
+Go to Feralas and accept [QA3380 The Sunken Temple][G74.4,43.4 Feralas]
+Go to Tanaris and turn in [QT3380 The Sunken Temple][G52.6,45.8 Tanaris], and accept followup [QA3444 The Stone Circle]
+Go to Un'Goro Crater and accept [QA4145 Larion and Muigin][G45.6,8.6 Un'Goro Crater]
+Complete [QC4145 Larion and Muigin]
+Turn in [QT4145 Larion and Muigin][G45.6,8.6 Un'Goro Crater], and accept followup [QA4147 Marvon's Workshop]
+Go to Ratchet in The Barrens and turn in [QT4147 Marvon's Workshop][G62.4,38.6 The Barrens], and accept followup [QA4146 Zapper Fuel]
+In Rachet complete [QC3444 The Stone Circle][G62.5,38.5 The Barrens]. It's in a small chest outside the door in the same house as previous quest.
+Go to Tanaris and turn in [QT3444 The Stone Circle][G52.6,45.8 Tanaris]
+Accept followup [QA3447 Secret of the Circle] and [QA3446 Into the Depths][G52.6,45.8 Tanaris]
+In Tanaris accept [QA4787 The Ancient Egg][G67,22.4 Tanaris] This quest is a followup to *The Prophecy of Mosharu* which is a part of the Zul Farrak guide, and needs to be completed first.
+
+Go to Swamp of Sorrows and accept [QA1424 Pool of Tears][G48,55 Swamp of Sorrows]
+Complete [QC1424 Pool of Tears]
+Turn in [QT1424 Pool of Tears][G48,55 Swamp of Sorrows], and accept followup [QA1429 The Atalai Exile]
+Go to The Hinterlands and turn in [QT1429 The Atalai Exile][G33.6,75.2 The Hinterlands], and accept followup [QA1444 Return to FelZerul] and [QA1446 Jammal'an the Prophet]
+In Hinterlands complete [QC4787 The Ancient Egg] This requires killing *allot* of Trolls, and can be done while making the ZF Mallet, and other elite quests in Hinterlands
+Go to Tanaris and turn in [QT4787 The Ancient Egg][G67,22.4 Tanaris], and accept followup [QA3528 The God Hakkar]
+
+Go to Swamp of Sorrows and turn in [QT1444 Return to FelZerul][G48,55 Swamp of Sorrows], and accept followup [QA1445 The Temple of AtalHakkar]
+
+
+
+Go to The Temple of Atal Hakkar, also known as Sunken Temple and complete:\\[QC1445 The Temple of AtalHakkar] The Fetish of Hakkar drops from any Atalai Elites inside and outside the dungeon\\[QC4146 Zapper Fuel] Drops from Deep Lurkers, Murk Worms, and Oozes\\[QT3447 Secret of the Circle] Order of serpent keys - South - North - Southwest - Southeast - Northwest - Northeast - Kill miniboss - touch snake statue\\[QT3446 Into the Depths] Complete at Altar of Hakkar\\[QC1446 Jammal'an the Prophet] Kill Jammal'an the Prophet\\[QA3373 The Essence of Eranikus] Shade of Eranikus drops Essence of Eranikus, accept this quest. Complete [QT3373 The Essence of Eranikus] in the room where the Eranikus is - Small alter thing\\[QC3528 The God Hakkar] Kill the Avatar of Hakkar which you summon by using the egg, he drops "Essence of Hakkar". Pick that up and right click it in your inventory to fill the egg with the Essence of Hakkar.
+
+Go to Swamp of Sorrows and turn in [QT1445 The Temple of AtalHakkar][G48,55 Swamp of Sorrows]
+Go to The Hinterlands and turn in [QT1446 Jammal'an the Prophet][G33.6,75.2 The Hinterlands]
+Go to Tanaris and turn in [QT3528 The God Hakkar][G67,22.4 Tanaris]. The followup The Lost Tablets of Mosh'aru is a part of the Blackrock Spire dungeon guide.
+Go to Un'Goro Crater and turn in [QT4146 Zapper Fuel][G45.6,8.6 Un'Goro Crater] 
+
+
+]], 'DungeonQuests - JanRK')
