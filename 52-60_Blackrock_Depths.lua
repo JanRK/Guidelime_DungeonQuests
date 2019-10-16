71827ace46fa3c53576b712db1e9822f2a54b7c0
JanRK <jk@itmatic.dk>
2019-10-16 19:00:03 +0200

    BRD

---
+++ 52-60_Blackrock_Depths.lua
@@ -0,0 +1,91 @@
+Guidelime.registerGuide([[
+[D Quests for Blackrock Depths\\ https://github.com/JanRK/Guidelime_DungeonQuests]
+[GA Horde]
+[N 52-60 Blackrock Depths]
+[NX 55-60 Lower Blackrock Spire.lua]
+
+
+
+First quest is a long neutral chain starting in Searing Gorge. End rewards is decent green items. Final quest is *A Taste of Flame*. \\
+Go to Searing Gorge and accept [QA3441 Divine Retribution] [G39,39 Searing Gorge]
+Talk to Kalaran and then complete [QT3441 Divine Retribution]. Accept followup [QA3442 The Flawless Flame] 
+Complete [QC3442 The Flawless Flame]
+Turn in [QT3442 The Flawless Flame][G39,39 Searing Gorge] and accept followup [QA3443 Forging the Shaft]
+Complete [QC3443 Forging the Shaft]
+Turn in [QT3443 Forging the Shaft][G39,39 Searing Gorge] and accept followup [QA3452 The Flame's Casing]
+Complete [QC3452 The Flame's Casing]
+Turn in [QT3452 The Flame's Casing][G39,39 Searing Gorge] and accept followup [QA3453 The Torch of Retribution]
+Wait for Kalaran Windblade to complete the Torch of Retribution, and turn in [QT3453 The Torch of Retribution][G39,39 Searing Gorge]. Accept followup [QA3454 The Torch of Retribution]
+Turn in [QT3454 The Torch of Retribution][G39,39 Searing Gorge] and accept followup [QA3462 Squire Maltrake]
+Turn in [QT3462 Squire Maltrake][G39,39 Searing Gorge] and accept followup [QA3463 Set Them Ablaze!]
+Complete [QC3463 Set Them Ablaze!]
+Turn in [QT3463 Set Them Ablaze!][G39,39 Searing Gorge] 
+Open the chest to accept [QA3481 Trinkets...][G39,38.9 Searing Gorge]
+Turn in [QT3481 Trinkets...][G39,38.9 Searing Gorge]
+Open the chest in your inventory to get the *Black Dragonflight Molt*\\
+Go to Burning Steppes and accept [QA4022 A Taste of Flame][G94.8,31.6 Burning Steppes]
+Turn in [QT4022 A Taste of Flame][G94.8,31.6 Burning Steppes] and accept followup [QA4024 A Taste of Flame]
+Accept [QA4123 The Heart of the Mountain][G65.2,23.8 Burning Steppes]
+
+
+
+Go to Badlands and pickup [QA4081 KILL ON SIGHT: Dark Iron Dwarves] [G3.8,47.5 Badlands]
+Accept [QA3906 Disharmony of Flame] [G3.4,48.2 Badlands]
+Accept [QA7201 The Last Element] [G3,47.6 Badlands]
+
+
+
+Accept [QA3801 Dark Iron Legacy]. You need to be dead to accept this quest. In ghost form, coming from the Searing Gorge, head into Blackrock Mountain and take a left till you come to a big chain. Take this chain across to the center. Go inside and go down 1 level and he will be on a pedestal.
+Talk to the ghost again, and turn in Accept [QT3801 Dark Iron Legacy]. Accept followup [QA3802 Dark Iron Legacy]. This is the quest for the *Shadowforge Key*.
+
+
+Go to BRD and complete:\\
+[QC4024 A Taste of Flame] Kill Bael'Gar and use the *Altered Black Dragonflight Molt* on his corpse\\
+[QC4081 KILL ON SIGHT: Dark Iron Dwarves]\\
+[QC3906 Disharmony of Flame] Slay Overmaster Pyron\\
+[QC7201 The Last Element] Loot quest item from Fire Elementals
+
+
+
+
+
+
+
+In Badlands, turn in [QT4081 KILL ON SIGHT: Dark Iron Dwarves] [G5.8,47.4 Badlands]
+Turn in [QT3906 Disharmony of Flame] [G3.4,48.2 Badlands] and accept followup [QA3907 Disharmony of Fire] 
+Turn in [QT7201 The Last Element] [G3,47.6 Badlands]
+Accept [QA4082 KILL ON SIGHT: High Ranking Dark Iron Officials][G4,46.8 Badlands] 
+Accept [QA3981 Commander Gor'shak][G5.8,47.6 Badlands] 
+Go to Burning Steppes and turn in [QT4024 A Taste of Flame][G94.8,31.6 Burning Steppes]
+
+
+Go to BRD and complete:\\
+[QT3981 Commander Gor'shak] Find Gor'Shak. You'll need the key from High Interrogator Gerstahn\\
+*Manually accept his followup once all have complete the quest* Defend Gor'shak\\
+After definding Gorshak, accept [QA4001 What Is Going On?]\\
+Talk to the dwarf in the cell across from Gorshak and complete [QC4001 What Is Going On?]\\
+[QC4082 KILL ON SIGHT: High Ranking Dark Iron Officials]\\
+[QC3907 Disharmony of Fire] Slay Lord Incendius and loot table from him\\
+[QC4123 The Heart of the Mountain] Open the coffers in the vault to spawn *Watchman Doomgrip*. Use his key to loot Heart of the Mountain in the vault
+[QT3802 Dark Iron Legacy] Kill Fineous Darkvire and bring Ironfel to the Shrine of Thaurissan.
+
+
+
+
+In Badlands, turn in [QT4082 KILL ON SIGHT: High Ranking Dark Iron Officials] [G5.8,47.4 Badlands] 
+Turn in [QT3907 Disharmony of Fire] [G3.4,48.2 Badlands]
+Go to Thrall in Orgrimmar and turn in [QT4001 What Is Going On?] [G32,37.8 Orgrimmar] and accept followup [QA4002 The Eastern Kingdoms]
+Talk to Thrall again to complete [QT4002 The Eastern Kingdoms] and accept followup [QA4003 The Royal Rescue]
+
+
+
+
+Go to BRD and complete:\\
+[QC4003 The Royal Rescue] Slay Emperor Dagran Thaurissan\\
+Talk to Princess Moira Bronzebeard to complete [QT4003 The Royal Rescue] and accept followup [QA4004 The Princess Saved?]
+
+
+Go to Thrall in Orgrimmar and turn in [QT4004 The Princess Saved?] [G32,37.8 Orgrimmar]
+
+
+]], 'DungeonQuests - JanRK')
