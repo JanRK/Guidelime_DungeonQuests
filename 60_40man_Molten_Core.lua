1ff1c63ebbfe39c89e6844ff4828c3736a3e3d53
JanRK <jk@itmatic.dk>
2019-11-06 15:23:16 +0100

    Molten Core

---
+++ 60_40man_Molten_Core.lua
@@ -0,0 +1,13 @@
+Guidelime.registerGuide([[
+[D Quests for Molten Core\\ https://github.com/JanRK/Guidelime_DungeonQuests]
+[GA Horde]
+[N 60 Molten Core]
+
+Go to the bottom of Blackrock Mountain and accept [QA7848 Attunement to the Core]
+
+Go to BRD and complete [QC7848 Attunement to the Core]. Look up *Lava Run* for a fast way to complete this.
+
+Go to the bottom of Blackrock Mountain and turn in [QT7848 Attunement to the Core]
+
+
+]], 'DungeonQuests - JanRK')
