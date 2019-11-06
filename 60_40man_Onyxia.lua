8abcc220dc83fde484492619d22c26ce824116fa
JanRK <jk@itmatic.dk>
2019-11-06 15:23:28 +0100

    Onyxia

---
+++ 60_40man_Onyxia.lua
@@ -0,0 +1,52 @@
+Guidelime.registerGuide([[
+[D Quests for Onyxia\\ https://github.com/JanRK/Guidelime_DungeonQuests]
+[GA Horde]
+[N 60 Onyxia]
+
+
+Turn in [QT3907 Disharmony of Fire] [G3.4,48.2 Badlands]
+Accept [QA3981 Commander Gor'shak][G5.8,47.6 Badlands] 
+
+
+Go to Badlands and accept talk to Warlord Goretooth to recieve quest item. Use it to accept [QA4903 Warlord's Command] [G5.8,47.4 Badlands] 
+
+[QC4903 Warlord's Command] Have to kill 3 bosses in LBRS and find a document that randomly spawn at one of them
+
+In Badlands turn in [QT4903 Warlord's Command] [G5.8,47.4 Badlands]. Accept followup [QA4941 Eitrigg's Wisdom]
+
+Go to Thrall's room in Orgrimmar, and talk to Eitrigg to complete [QC4941 Eitrigg's Wisdom]
+Turn in [QT4941 Eitrigg's Wisdom] [G32,37.8 Orgrimmar] and accept followup [QA4974 For The Horde!]
+
+[QC4974 For The Horde!] Travel to UBRS and slay Warchief Rend Blackhand. Take his head and return to Orgrimmar
+
+Go to Thrall in Orgrimmar and turn in [QT4974 For The Horde!] [G32,37.8 Orgrimmar] and accept followup [QA6566 What the Wind Carries]
+Talk to Thrall to complete [QC6566 What the Wind Carries]
+Turn in [QT6566 What the Wind Carries] and accept followup [QA6567 The Champion of the Horde]
+Find Rexxar and turn in [QT6567 The Champion of the Horde]. Rexxar patrols from Feralas through Desolace to Stonetalon Mountains. 
+Accept [QA6568 The Testament of Rexxar] from Rexxar.
+Go to Western Plaguelands and turn in [QT6568 The Testament of Rexxar][G50.8,77.8 Western Plaguelands] and accept followup [QA6569 Oculus Illusions]
+
+[QC6569 Oculus Illusions] Travel to UBRS and collect the *Black Dragonspawn Eye* from the Dragonkins there
+
+Return to Western Plaguelands and turn in [QT6569 Oculus Illusions][G50.8,77.8 Western Plaguelands] and accept followup [QA6570 Emberstrife]
+
+Go to Dustwallow Marsh and wear the *Amulet of Draconic Subversion* to turn in [QT6570 Emberstrife][G56.6,87.6 Dustwallow Marsh] \\ Accept [QA6584 The Test of Skulls, Chronalis] \\ Accept [QA6582 The Test of Skulls, Scryer] \\ Accept [QA6583 The Test of Skulls, Somnus]
+
+Find a group/raid and kill the 3 dragons (plus 1 more after these):\\
+Complete [QC6584 The Test of Skulls, Chronalis] in Tanaris\\ 
+Complete [QC6582 The Test of Skulls, Scryer] in Winterspring \\ 
+Complete [QC6583 The Test of Skulls, Somnus] in Swamp of Sorrows \\
+
+Go to Dustwallow Marsh and wear the *Amulet of Draconic Subversion* to turn in [QT6584 The Test of Skulls, Chronalis], [QT6582 The Test of Skulls, Scryer],[QT6583 The Test of Skulls, Somnus][G56.6,87.6 Dustwallow Marsh] and accept followup [QA6585 The Test of Skulls, Axtroz]
+
+[QC6585 The Test of Skulls, Axtroz] in Wetlands
+
+Go to Dustwallow Marsh and wear the *Amulet of Draconic Subversion* to turn in [QT6585 The Test of Skulls, Axtroz][G56.6,87.6 Dustwallow Marsh] and accept followup [QA6601 Ascension...]
+
+Find Rexxar again and turn in [QT6601 Ascension...]. Rexxar patrols from Feralas through Desolace to Stonetalon Mountains. Accept followup [QA6602 Blood of the Black Dragon Champion]
+
+[QC6602 Blood of the Black Dragon Champion] Travel to UBRS and kill General Drakkisath to get his blood
+
+Find Rexxar again and turn in [QT6602 Blood of the Black Dragon Champion] and you're done!
+
+]], 'DungeonQuests - JanRK')
