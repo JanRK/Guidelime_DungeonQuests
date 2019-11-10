1f06a5008dff987ed8049b0ed6f97c23b649fb22
JanRK <jk@itmatic.dk>
2019-11-11 00:23:48 +0100

    First try at undead

---
+++ 58-60_Stratholme_Undead.lua
@@ -0,0 +1,55 @@
+Guidelime.registerGuide([[
+[D Quests for Stratholme Undead\\ https://github.com/JanRK/Guidelime_DungeonQuests]
+[GA Horde]
+[N 58-60 Stratholme Undead]
+
+Go to Eastern Plaguelands and pickup [QA6133 The Ranger Lord's Behest][G26.6,74.8 Eastern Plaguelands]
+Complete [QC6133 The Ranger Lord's Behest]
+Turn in [QT6133 The Ranger Lord's Behest][G26.6,74.8 Eastern Plaguelands]. Accept followup [QA6135 Duskwing, Oh How I Hate Thee...]
+Complete [QC6135 Duskwing, Oh How I Hate Thee...]
+Turn in [QT6135 Duskwing, Oh How I Hate Thee...][G26.6,74.8 Eastern Plaguelands]. Accept followup [QA6163 Ramstein]
+
+
+Go to Western Plaguelands and pickup [QA5461 The Human, Ras Frostwhisper][G70.4,74 Western Plaguelands] This quest requires part of a long chain that starts and ends in Scholomance. Prereq and last part is in Scholomance guide.
+Complete [QC5461 The Human, Ras Frostwhisper] in Arathi Highlands
+Turn in [QT5461 The Human, Ras Frostwhisper][G70.4,74 Western Plaguelands]. Accept followup [QA5462 The Dying, Ras Frostwhisper]
+Turn in [QT5462 The Dying, Ras Frostwhisper][G81.6,57.8 Eastern Plaguelands]. Accept followup [QA5463 Menethil's Gift]
+
+Go to Lights Hope Chapel in Eastern Plaguelands and pickup:\\
+[QA5212 The Flesh Does Not Lie][G81.4,59.6 Eastern Plaguelands]\\
+[QA5243 Houses of the Holy][G81.6,57.8 Eastern Plaguelands]
+
+If you already done Live quests, you can pickup followup [QA5263 Above and Beyond][G81.4,59.8 Eastern Plaguelands]
+
+Go to Stratholme Undead:\\
+Accept [QA5122 The Medallion of Faith] and turn it in again. The objective for this quest is in *Stratholme Live* in a chest behind the boss Malor the Zealous\\
+Complete [QC5212 The Flesh Does Not Lie] Recover 20 Plagued Flesh Samples\\
+Complete [QC5243 Houses of the Holy] Loot 5 Stratholme Holy Water from crates scattered around the dungeon\\
+Complete [QC6163 Ramstein] Kill Ramstein the Gorger
+
+Complete [QT5463 Menethil's Gift] Kill Baron and you will see a pentagram on the ground that you can interact with. Accept [QA5464 Menethil's Gift] 
+
+Return to Lights Hope Chapel in Eastern Plaguelands and turn in [QT5212 The Flesh Does Not Lie][G81.4,59.6 Eastern Plaguelands]. Accept followup [QA5213 The Active Agent]
+Turn in [QT5243 Houses of the Holy][G81.6,57.8 Eastern Plaguelands]
+Turn in [QT5464 Menethil's Gift][G81.6,57.8 Eastern Plaguelands]. Accept followup [QA5465 Soulbound Keepsake]
+
+Turn in [QT6163 Ramstein][G26.6,74.8 Eastern Plaguelands].
+
+Go to Stratholme Undead and complete:\\
+[QC5213 The Active Agent] Loot a very small box that ramdomly spawn in either Nerub'Enkan's Ziggurat, Baroness Anastari's Ziggurat or Maleki's Ziggurat\\
+[QC5263 Above and Beyond] Kill Baron Rivendare
+
+
+
+
+Return to Lights Hope Chapel in Eastern Plaguelands and turn in [QT5212 The Flesh Does Not Lie][G81.4,59.6 Eastern Plaguelands].
+Turn in [QT5263 Above and Beyond][G81.4,59.8 Eastern Plaguelands]. Accept followup [QA5264 Lord Maxwell Tyrosus]
+Turn in [QT5264 Lord Maxwell Tyrosus][G81.6,58 Eastern Plaguelands]. Accept followup [QA5265 The Argent Hold]
+Turn in [QT5265 The Argent Hold][G81.8,58 Eastern Plaguelands] at the box right behind Lord Maxwell
+Turn in [QT5213 The Active Agent][G81.4,59.6 Eastern Plaguelands]
+
+Go to Western Plaguelands and turn in [QT5465 Soulbound Keepsake][G70.4,74 Western Plaguelands]. Followup is final part of the chain for Scholomance with some decent rewards.
+
+
+
+]], 'DungeonQuests - JanRK')
