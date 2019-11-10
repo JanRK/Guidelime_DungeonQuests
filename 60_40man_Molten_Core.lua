e37c453ab96907170e813dd4aab1e5e8e6245ad1
JanRK <jk@itmatic.dk>
2019-11-11 00:24:12 +0100

    More MC

--- 60_40man_Molten_Core.lua
+++ 60_40man_Molten_Core.lua
@@ -3,11 +3,31 @@ Guidelime.registerGuide([[
 [GA Horde]
 [N 60 Molten Core]
 
-Go to the bottom of Blackrock Mountain and accept [QA7848 Attunement to the Core]
+To get attuned to MC, go to the bottom of Blackrock Mountain and accept [QA7848 Attunement to the Core]
 
 Go to BRD and complete [QC7848 Attunement to the Core]. Look up *Lava Run* for a fast way to complete this.
 
-Go to the bottom of Blackrock Mountain and turn in [QT7848 Attunement to the Core]
+Go to the bottom of Blackrock Mountain and turn in [QT7848 Attunement to the Core], and you can now raid MC.
+
+Go to Azshara and pickup [QA6805 Stormers and Rumblers] and [QA6804 Poisoned Water][G79.2,73.6 Azshara]\\
+This quest line if for getting *Eternal Quintessence* which is used to dowse the runes in MC to spawn Majordomo
+
+Complete:\\
+[QC6804 Poisoned Water] Eastern Plaguelands - Use the Aspect of Neptulon on poisoned elementals\\
+[QA6805 Stormers and Rumblers] Silithus - Kill 15 Dust Stormers and 15 Desert Rumbers
+
+Return to Azshara and turn in [QT6805 Stormers and Rumblers] and [QT6804 Poisoned Water][G79.2,73.6 Azshara]. Accept followup [QA6821 Eye of the Emberseer]
+[QC6821 Eye of the Emberseer] Go to UBRS, kill Pyroguard Emberseer and loot eye
+Return to Azshara and turn in [QT6821 Eye of the Emberseer][G79.2,73.6 Azshara]. Accept followup [QA6822 The Molten Core]
+Go to MC and kill some trash to complete [QC6822 The Molten Core]
+Return to Azshara and turn in [QT6822 The Molten Core][G79.2,73.6 Azshara]. Accept followup [QA6823 Agent of Hydraxis]
+To complete [QC6823 Agent of Hydraxis] you need to be Honored with *Hydraxian Waterlords* which takes quite allot of killing stuff in MC. You can also grind it outside of MC.
+Return to Azshara and turn in [QT6823 Agent of Hydraxis][G79.2,73.6 Azshara]. Accept followup [QA6824 Hands of the Enemy]
+Go to MC and complete [QC6824 Hands of the Enemy]
+Return to Azshara and turn in [QT6824 Hands of the Enemy][G79.2,73.6 Azshara]. Accept followup [QA7486 A Hero's Reward]
+You can now get Aqual Quintessence\\
+Turn in [QT7486 A Hero's Reward][G79.2,74.8 Azshara] at the chest under the water at the southern base of the island.
+
 
 
 ]], 'DungeonQuests - JanRK')
